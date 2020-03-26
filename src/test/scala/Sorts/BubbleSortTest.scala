e1207f508d98c4e634bb48bb9f579204af6171cf
CanftIn <wwc7033@gmail.com>
2020-03-26 22:36:44 +0800

    Update BubbleSortTest.scala

--- src/test/scala/Sorts/BubbleSortTest.scala
+++ src/test/scala/Sorts/BubbleSortTest.scala
@@ -3,6 +3,8 @@ package Sorts
 import org.scalatest._
 
 class BubbleSortTest extends FlatSpec {
-  val arr = Array(3,2,7,1,9,0)
-  assert(BubbleSort.bubbleSort(arr) === Array(0,1,2,3,7,9))
+  "A Bubble Sort" should "return a sorted version of an array passed to it" in {
+    val arr = Array(3,2,7,1,9,0)
+    assert(BubbleSort.bubbleSort(arr) === Array(0,1,2,3,7,9))
+  }
 }
