0d6423b3206f0549bb6d7ce8b4fc10aaec773e97
CanftIn <wwc7033@gmail.com>
2020-03-26 22:30:20 +0800

    update insertionSort

---
+++ src/test/scala/Sorts/InsertionSortTest.scala
@@ -0,0 +1,10 @@
+package Sorts
+
+import org.scalatest._
+
+class InsertionSortTest extends FlatSpec {
+  "A Bubble Sort" should "return a sorted version of an array passed to it" in {
+    val arr = Array(3,2,7,1,9,0)
+    assert(BubbleSort.bubbleSort(arr) === Array(0,1,2,3,7,9))
+  }
+}
